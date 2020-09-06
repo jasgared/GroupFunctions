2634eb995a5707f05182b5a08e84c7e170314d76
Jaswanth Garre <jasgared@gmail.com>
2020-09-06 09:05:07 -0500

    Added second function in Justprint

--- GroupFunctions/Function1.cs
+++ GroupFunctions/Function1.cs
@@ -15,8 +15,8 @@ namespace GroupFunctions
     public static class JustPrint
     {
         [FunctionName("JustPrint")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public static async Task<IActionResult> FirstFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -45,5 +45,38 @@ namespace GroupFunctions
             //Console.WriteLine(response);
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("JustPrint2")]
+        public static async Task<IActionResult> SecondFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            // Two operators here
+            // 1. ?? --> null-coalescing operator --> op1 ?? op2
+            // Takes op1 value if its not-null
+            // else takes the op2 value.
+            // 2. ? --> implies that the name of data object can be null.
+            name = name ?? data?.name;
+
+            // This is usual terinary operator
+            // Checking for name if null or not and printing message accordingly.
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string data1 = "@{'number':'11'}";
+            //req.Body = data1;
+            HttpRequestMessage httpreq = new HttpRequestMessage();
+            httpreq.Content = new StringContent("{\"number\":\"11\"}", Encoding.UTF8, "application/json");
+            //var response = ConditionalFunction.Run(httpreq, log);
+            //Console.WriteLine(response);
+            return new OkObjectResult(responseMessage);
+        }
+
     }
 }
